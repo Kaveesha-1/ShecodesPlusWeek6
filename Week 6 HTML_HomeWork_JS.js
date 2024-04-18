2a735f6b917744ee36b38efd9b055a3c4f4265d9
Kaveesha-1 <Kaveeshanaicker@gmail.com>
2024-04-18 15:50:46 +0200

    ApiKey added

--- Week 6 HTML_HomeWork_JS.js
+++ Week 6 HTML_HomeWork_JS.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "f76a11dtae0639b478d784b09d743ob";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
